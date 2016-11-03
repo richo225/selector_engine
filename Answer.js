d6a23fdcdf120cddff9fe81467798a2025ef3988
richo225 <richo225@gmail.com>
2016-11-03 23:09:47 +0000

    5 tests pass. All ifelse statements are now in functions

--- Answer.js
+++ Answer.js
@@ -8,50 +8,55 @@ function $(selector) {
   // G: $("div.some_class#some_id")
 
 	// check if lone id or class
-
 	if (selector.startsWith(".")) {
 		return document.getElementsByClassName(selector.substr(1));
 	} else if (selector.startsWith("#")) {
 		// id's are unique so need to manually place inside array;
 		return [document.getElementById(selector.substr(1))];
 	} else {
-		return classTest();
+		return tagCheck();
 	}
 
-	function classTest() {
+	function tagCheck() {
 		if ((selector.includes(".") === false) && (selector.includes("#") === false)) {
 			// lone tag
 			console.log("returning lone tag of" + document.getElementsByTagName(selector));
 			return document.getElementsByTagName(selector);
 		} else if ((selector.includes(".") === true) && (selector.includes("#") === false)) {
 			// tag and class
-			var cTagArray = [];
-			var cTag = selector.split(".")[0];
-			var cTagClass = selector.split(".")[1];
-			var cAllTags = [...document.getElementsByTagName(cTag)];
-			cAllTags.forEach(function(element){
-				if (element.classList.contains(cTagClass)) {
-					cTagArray.push(element);
-				}
-			});
-			return cTagArray;
+			return singleClass();
 		} else if ((selector.includes(".") === false) && (selector.includes("#") === true)) {
 			// tag and id
-			var iTagArray = [];
-			var iTag = selector.split("#")[0];
-			var iTagId = selector.split("#")[1];
-			var iAllTags = [...document.getElementsByTagName(iTag)];
-			iAllTags.forEach(function(element){
-				if (element.id === iTagId) {
-					iTagArray.push(element);
-				}
-			});
-			console.log("returning id array of" +iTagArray);
-			return iTagArray;
+			return singleId();
 		} else {
-			// tag and class and id
-			// tagMultiple(selector);
 			return [];
 		}
 	}
+
+	function singleClass() {
+		var tagArray = [];
+		var tag = selector.split(".")[0];
+		var tagClass = selector.split(".")[1];
+		var allTags = [...document.getElementsByTagName(tag)];
+		allTags.forEach(function(element){
+			if (element.classList.contains(tagClass)) {
+				tagArray.push(element);
+			}
+		});
+		return tagArray;
+	}
+
+	function singleId() {
+		var tagArray = [];
+		var tag = selector.split("#")[0];
+		var tagId = selector.split("#")[1];
+		var allTags = [...document.getElementsByTagName(tag)];
+		allTags.forEach(function(element){
+			if (element.id === tagId) {
+				tagArray.push(element);
+			}
+		});
+		return tagArray;
+	}
+
 }
