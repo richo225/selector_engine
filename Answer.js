34309f73a69423a48409836d86c3f3a7f9e4abcd
richo225 <richo225@gmail.com>
2016-11-04 13:21:21 +0000

    Completed 7/7 and refactored

--- Answer.js
+++ Answer.js
@@ -1,17 +1,10 @@
-function $(selector) {
-	// A: $("div"),
-  // B: $("img.some_class"),
-  // C: $("#some_id"),
-  // D: $(".some_class"),
-  // E: $("input#some_id"),
-  // F: $("div#some_id.some_class"),
-  // G: $("div.some_class#some_id")
+var $ =	function (selector) {
 
-	// check if lone id or class
+	var elements = [];
+
 	if (selector.startsWith(".")) {
 		return document.getElementsByClassName(selector.substr(1));
 	} else if (selector.startsWith("#")) {
-		// id's are unique so need to manually place inside array;
 		return [document.getElementById(selector.substr(1))];
 	} else {
 		return tagCheck();
@@ -19,44 +12,59 @@ function $(selector) {
 
 	function tagCheck() {
 		if ((selector.includes(".") === false) && (selector.includes("#") === false)) {
-			// lone tag
-			console.log("returning lone tag of" + document.getElementsByTagName(selector));
 			return document.getElementsByTagName(selector);
 		} else if ((selector.includes(".") === true) && (selector.includes("#") === false)) {
-			// tag and class
 			return singleClass();
 		} else if ((selector.includes(".") === false) && (selector.includes("#") === true)) {
-			// tag and id
 			return singleId();
 		} else {
-			return [];
+			return multipleCheck();
 		}
 	}
 
 	function singleClass() {
-		var tagArray = [];
 		var tag = selector.split(".")[0];
 		var tagClass = selector.split(".")[1];
 		var allTags = [...document.getElementsByTagName(tag)];
 		allTags.forEach(function(element){
 			if (element.classList.contains(tagClass)) {
-				tagArray.push(element);
+				elements.push(element);
 			}
 		});
-		return tagArray;
+		return elements;
 	}
 
 	function singleId() {
-		var tagArray = [];
 		var tag = selector.split("#")[0];
 		var tagId = selector.split("#")[1];
 		var allTags = [...document.getElementsByTagName(tag)];
 		allTags.forEach(function(element){
 			if (element.id === tagId) {
-				tagArray.push(element);
+				elements.push(element);
 			}
 		});
-		return tagArray;
+		return elements;
 	}
 
-}
+	function multipleCheck() {
+		if (selector.indexOf(".") < selector.indexOf("#")) {
+			return multipleSelector(".", "#", 0, 1);
+		} else {
+			return multipleSelector("#", ".", 1, 0);
+		}
+	}
+
+	function multipleSelector(symb1, symb2, splitClass, splitId) {
+		var tag = selector.split(symb1)[0];
+		var tagRemain = selector.split(symb1)[1];
+		var tagClass = tagRemain.split(symb2)[splitClass];
+		var tagId = tagRemain.split(symb2)[splitId];
+		var allTags = [...document.getElementsByTagName(tag)];
+		allTags.forEach(function(element){
+			if ((element.id === tagId) && (element.classList.contains(tagClass))) {
+				elements.push(element);
+			}
+		});
+		return elements;
+	}
+};
